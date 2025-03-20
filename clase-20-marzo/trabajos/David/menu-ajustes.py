db5e42f5ea428c31d6e362b81562e7f4884f58df
MatiasGaming03 <mati7865774@gmail.com>
2025-03-20 09:03:04 -0300

    Create menu-ajustes.py
    Menu ajustes de david

---
+++ clase-20-marzo/trabajos/David/menu-ajustes.py
@@ -0,0 +1,12 @@
+print("Multijugador,Opciones,Solitario,Lenguaje")
+N=input("elija una opción:")
+if N=="Opciones":
+    print("Bienvenido a Opciones")
+elif N=="Multijugador":
+    print("Bienvenido a multijugador")
+elif N=="Solitario":
+    print("Bienvenido a Solitario")
+elif N=="Lenguaje":
+    print("Bienvenido a Lenguaje")
+else:
+    print("Error")
