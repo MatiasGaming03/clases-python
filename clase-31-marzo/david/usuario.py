dce673362d16416b5cc8dd5e2bc0528bf0164f28
Davidcardozo1609 <davidcardozo1609@gmail.com>
2025-03-31 21:24:30 -0300

    Create usuario.py

---
+++ clase-31-marzo/david/usuario.py
@@ -0,0 +1,6 @@
+usuario=input("ingrese el usuario por favor")
+contra=input("ingrese la contraseña por favor")
+if usuario=="davidpro" and contra=="minecraft":
+    print("acceso valido")
+else:
+    print("acceso denegado")
