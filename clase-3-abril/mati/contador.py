05520d24435e0d51d5e11e2dfced7a52478b1302
MatiasGaming03 <mati7865774@gmail.com>
2025-04-03 09:45:02 -0300

    Create contador.py

---
+++ clase-3-abril/mati/contador.py
@@ -0,0 +1,9 @@
+num = float(input("Ingresa un numero: "))
+suma = 0
+
+while num != 0:
+    
+    suma = suma + num
+    num = float(input("Ingresa un numero: "))
+    
+print("La suma de los numeros es:", suma)
