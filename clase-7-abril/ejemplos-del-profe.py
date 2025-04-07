639f1e638266ff3a49839418b0c8ed3ed878788f
MatiasGaming03 <mati7865774@gmail.com>
2025-04-07 19:52:47 -0300

    Update ejemplos-del-profe.py

--- clase-7-abril/ejemplos-del-profe.py
+++ clase-7-abril/ejemplos-del-profe.py
@@ -4,7 +4,6 @@
 for x in range(6):
     print(x)
 
-
 #tiene 2 paramentros: el inicio y el fin
 for x in range(0, 6):
     print(x)
@@ -13,8 +12,6 @@ for x in range(0, 6):
 #ejemplo (de dos en dos): 2, 4, 6, 8,...
 for x in range(0, 6, 1):
     print(x)
-
-
 
 #ciclo FOR
 for x in range(0, 21, 2):
