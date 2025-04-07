3057b8334d52962a2f26d2a5415c21e8290c0fa1
MatiasGaming03 <mati7865774@gmail.com>
2025-04-07 19:52:30 -0300

    Update ejemplos-del-profe.py

--- clase-7-abril/ejemplos-del-profe.py
+++ clase-7-abril/ejemplos-del-profe.py
@@ -1,3 +1,21 @@
+#ejemplos de distintos metodos
+
+#solo tiene un parametro: el final al que se le resta 1
+for x in range(6):
+    print(x)
+
+
+#tiene 2 paramentros: el inicio y el fin
+for x in range(0, 6):
+    print(x)
+
+#tiene 3 parametros: inicio, fin y de cuanto en cuanto
+#ejemplo (de dos en dos): 2, 4, 6, 8,...
+for x in range(0, 6, 1):
+    print(x)
+
+
+
 #ciclo FOR
 for x in range(0, 21, 2):
     print(x)
